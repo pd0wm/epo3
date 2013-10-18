b8595250ce58cddd1bcb0ba48e47e02264157194
Willem Melching <willem.melching@gmail.com>
2013-10-18 11:59:47 +0200

    updated entities

--- timer.vhd
+++ timer.vhd
@@ -3,11 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity timer is
 	port(
-		clk   : in  std_logic;
-		rst   : in  std_logic;
+		clk     : in  std_logic;
+		rst     : in  std_logic;
 
-		time  : in  std_logic_vector(31 downto 0);
-		start : in  std_logic;
-		done  : out std_logic
+		vga_clk : in  std_logic;
+		time    : in  std_logic_vector(7 downto 0);
+		start   : in  std_logic;
+		done    : out std_logic
 	);
 end timer;
