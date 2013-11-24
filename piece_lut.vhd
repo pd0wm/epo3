4afb72c0cc6b6d87f689bb6eccb3d951c925319f
Tongdongq <tongdongq@gmail.com>
2013-11-24 11:53:11 +0100

    Update piece_lut.vhd
    De entity piece_lut heet nu lut. Laat me weten als dit een probleem is.

--- piece_lut.vhd
+++ piece_lut.vhd
@@ -1,23 +1,25 @@
-library ieee;
-use ieee.std_logic_1164.all;
-entity piece_lut is
-	port(
-		clk        : in  std_logic;
-		rst        : in  std_logic;
+library IEEE;
+use IEEE.std_logic_1164.ALL;
 
-		mask       : out std_logic_vector(31 downto 0);
+entity lut is
+   port(clk	  :IN	 std_logic;
+	reset	  :IN	 std_logic;
+	start     :IN    std_logic;
+        rot       :IN    std_logic_vector(1 downto 0);
+        y         :IN    std_logic_vector(3 downto 0);
+        x         :IN    std_logic_vector(2 downto 0);
+        p_type    :IN    std_logic_vector(2 downto 0);
+        rom_in    :IN    std_logic_vector(3 downto 0);
+        pixel     :IN    std_logic_vector(1 downto 0);
+	rom_out	  :OUT	 std_logic_vector(6 downto 0);
+        ready     :OUT   std_logic;
+        error_side:OUT   std_logic;
+        error_bot :OUT   std_logic;
+        ram	  :OUT   std_logic_vector(7 downto 0));
+end lut;
 
-		x          : in  std_logic_vector(3 downto 0);
-		y          : in  std_logic_vector(4 downto 0);
-		rot        : in  std_logic_vector(1 downto 0);
-		piece_type : in  std_logic_vector(2 downto 0);
-		ready      : out std_logic;
-		error      : out std_logic;
-		start      : in  std_logic;
 
-		rom_addr   : out std_logic_vector(7 downto 0);
-		rom_data   : in  std_logic_vector(15 downto 0)
-	);
-end piece_lut;
+
+
 
 
