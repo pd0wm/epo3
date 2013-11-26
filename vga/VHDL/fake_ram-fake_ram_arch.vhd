d31ede80fae3a1da7fc4596c3cdd79e174b766a7
Willem Melching <willem.melching@gmail.com>
2013-11-26 16:20:54 +0100

    created fake ram stub

---
+++ vga/VHDL/fake_ram-fake_ram_arch.vhd
@@ -0,0 +1,8 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+architecture fake_ram_arch of fake_ram is
+begin
+end fake_ram_arch;
+
+
---
+++ vga/VHDL/fake_ram.vhd
@@ -0,0 +1,7 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+entity fake_ram is
+end fake_ram;
+
+
