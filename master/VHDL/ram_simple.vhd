7e8948439133194a56dfef891e71792237297b93
Willem Melching <willem.melching@gmail.com>
2013-11-26 19:14:46 +0100

    Completed simple ram

--- master/VHDL/ram_simple.vhd
+++ master/VHDL/ram_simple.vhd
@@ -1,7 +1,18 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity ram_simple is
-end ram_simple;
+	port(
+		clk             : in    std_logic;
+		rst             : in    std_logic;
 
+		rw_addr         : in    std_logic_vector(7 downto 0);
+		rw_data         : inout std_logic;
+		rw_write_enable : in    std_logic;
+
+		ro_addr         : in    std_logic_vector(7 downto 0);
+		ro_out          : out   std_logic
+	);
+
+end ram_simple;
 
