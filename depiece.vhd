a368200d569f87fa950c62da40046978c43aef69
Willem Melching <willem.melching@gmail.com>
2013-11-26 09:43:14 +0100

    renamed enitities

---
+++ depiece.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity depiece is
+	port(
+		clk        : in    std_logic;
+		rst        : in    std_logic;
+
+		mask       : in    std_logic_vector(7 downto 0);
+		mask_select : out std_logic_vector(2 downto 0);
+
+		draw_erase : in    std_logic;   --draw = 1, erase = 0
+		ready      : out   std_logic;
+		start      : in    std_logic;
+
+		write      : out   std_logic;
+		addr       : out   std_logic_vector(7 downto 0);
+		data       : inout std_logic
+	);
+end depiece;
+
+
