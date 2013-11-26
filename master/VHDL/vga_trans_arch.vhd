8b34cd3df01e279cf6744b731fee704e6e1612b2
Willem Melching <willem.melching@gmail.com>
2013-11-26 18:15:08 +0100

    Added master project

---
+++ master/VHDL/vga_trans_arch.vhd
@@ -0,0 +1,45 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.vga_params.all;
+
+architecture vga_trans_arch of vga_trans is
+	signal counter_state, counter_new   : std_logic_vector(counter_block_width_len-1 downto 0);
+	signal mem_addr_state, mem_addr_new : std_logic_vector(mem_addr_len - 1 downto 0);
+begin
+	mem_addr_out <= mem_addr_state;
+
+	process(in_field_in, counter_state, new_line_in, new_frame_in, mem_addr_state, mem_addr_reset_in)
+	begin
+		mem_addr_new <= mem_addr_state;
+		counter_new <= counter_state;
+	
+		if (in_field_in = '1') then
+			counter_new <= std_logic_vector(unsigned(counter_state) + 1);
+
+			if (unsigned(counter_state) = field_block_width - 1) then
+				-- Reset counter
+				counter_new <= (others => '0');
+
+				-- Increase mem_addr
+				mem_addr_new <= std_logic_vector(unsigned(mem_addr_state) + 1);
+			end if;
+		end if;
+
+		if (new_line_in = '1' or new_frame_in = '1') then
+			counter_new  <= (others => '0');
+			mem_addr_new <= mem_addr_reset_in;
+		end if;
+	end process;
+
+	process(clk, rst)
+	begin
+		if (rst = '1') then
+			counter_state  <= (others => '0');
+			mem_addr_state <= (others => '0');
+		elsif (clk'event and clk = '1') then
+			counter_state <= counter_new;
+			mem_addr_state <= mem_addr_new;
+		end if;
+	end process;
+end;
---
+++ master/VHDL/vga_triggers.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.vga_params.all;
+
+entity vga_triggers is
+	port(
+		clk           : in  std_logic;
+		rst           : in  std_logic;
+
+		pos_x_in      : in  std_logic_vector(pos_x_len - 1 downto 0);
+		pos_y_in      : in  std_logic_vector(pos_y_len - 1 downto 0);
+
+		new_line_out  : out std_logic;
+		new_frame_out : out std_logic;
+
+		end_line_out  : out std_logic;
+		end_frame_out : out std_logic
+	);
+end entity;
