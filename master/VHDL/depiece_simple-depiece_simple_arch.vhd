a95a004ef7ebdbd33d3d9dfeb572dc02f7dc552e
Willem Melching <willem.melching@gmail.com>
2013-11-26 19:54:46 +0100

    Fixed ram writing issue

--- master/VHDL/depiece_simple-depiece_simple_arch.vhd
+++ master/VHDL/depiece_simple-depiece_simple_arch.vhd
@@ -5,17 +5,22 @@ use work.vga_params.all;
 
 architecture depiece_simple_arch of depiece_simple is
 begin
-L1:	process(clk, rst)
+	L1 : process(clk, rst)
+		variable written : std_logic := '0';
+
 	begin
 		if (clk'event and clk = '1') then
 			if (rst = '1') then
-				write <= '0';
-				addr <= (others => '0');
+				write    <= '0';
+				addr     <= (others => '0');
 				data_out <= '0';
 			else
-				addr <= "00000001";
-				data_out <= '1';
-				write <= '1';			
+				if (written = '0') then
+					addr     <= "00000001";
+					data_out <= '1';
+					write    <= '1';
+					written  := '1';
+				end if;
 			end if;
 		end if;
 	end process;
