63f26445e4c84d2906dfda4d32446efc7117292e
Tongdongq <tongdongq@gmail.com>
2013-11-29 13:17:08 +0100

    data bestaat uit twee porten

--- check_mask.vhd
+++ check_mask.vhd
@@ -1,22 +1,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity check_mask is
-	port(
-		clk         : in    std_logic;
-		rst         : in    std_logic;
+        port(
+                clk : in std_logic;
+                rst : in std_logic;
 
-		mask        : in    std_logic_vector(7 downto 0);
-		mask_select : out   std_logic_vector(1 downto 0);
+                mask : in std_logic_vector(7 downto 0);
+                mask_select : out std_logic_vector(1 downto 0);
 
-		ready       : out   std_logic;
-		empty       : out   std_logic;
+                ready : out std_logic;
+                empty : out std_logic;
 
-		start       : in    std_logic;
+                start : in std_logic;
 
-		write       : out   std_logic;
-		addr        : out   std_logic_vector(7 downto 0);
-		data        : inout std_logic
-	);
+                write : out std_logic;
+                addr : out std_logic_vector(7 downto 0);
+                data_in : in std_logic;
+                data_out : out std_logic
+        );
 end check_mask;
-
-
