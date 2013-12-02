9e734ef82f665ef1eb872eb342d43bdbec7aafad
Willem Melching <willem.melching@gmail.com>
2013-12-02 20:32:00 +0100

    Adeed controller configurations

---
+++ master/VHDL/controller_tb_controller_tb_arch_cfg.vhd
@@ -0,0 +1,8 @@
+configuration controller_tb_controller_tb_arch_cfg of controller_tb is
+   for controller_tb_arch
+      for all: controller use configuration work.controller_controller_arch_cfg;
+      end for;
+   end for;
+end controller_tb_controller_tb_arch_cfg;
+
+
