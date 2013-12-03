5e0210c70bfc53b9b4b64f95244c3bceb93db0d4
Tong Dong <tongdongq@gmail.com>
2013-12-03 12:27:26 +0100

    LUT vhdl working, not synthesizable

--- LUT/SLS/piece_lut.sls
+++ LUT/SLS/piece_lut.sls
@@ -1,94 +1,92 @@
 extern network dfr11 (terminal D, R, CK, Q, vss, vdd)
 extern network no210 (terminal A, B, Y, vss, vdd)
-extern network na310 (terminal A, B, C, Y, vss, vdd)
-extern network ex210 (terminal A, B, Y, vss, vdd)
+extern network mu111 (terminal A, B, S, Y, vss, vdd)
 extern network na210 (terminal A, B, Y, vss, vdd)
-extern network no310 (terminal A, B, C, Y, vss, vdd)
+extern network ex210 (terminal A, B, Y, vss, vdd)
 extern network iv110 (terminal A, Y, vss, vdd)
-network piece_lut (terminal lut_clk, lut_rst, lut_mask_7_0_7, lut_mask_7_0_6, 
-                            lut_mask_7_0_5, lut_mask_7_0_4, lut_mask_7_0_3, 
-                            lut_mask_7_0_2, lut_mask_7_0_1, lut_mask_7_0_0, 
-                            lut_mask_select_1_0_1, lut_mask_select_1_0_0, 
-                            lut_x_2_0_2, lut_x_2_0_1, lut_x_2_0_0, lut_y_3_0_3, 
-                            lut_y_3_0_2, lut_y_3_0_1, lut_y_3_0_0, 
-                            lut_rot_1_0_1, lut_rot_1_0_0, lut_piece_type_2_0_2, 
-                            lut_piece_type_2_0_1, lut_piece_type_2_0_0, 
-                            lut_next_piece, lut_ready, lut_error_side, 
-                            lut_error_bot, lut_start, lut_rom_addr_6_0_6, 
-                            lut_rom_addr_6_0_5, lut_rom_addr_6_0_4, 
-                            lut_rom_addr_6_0_3, lut_rom_addr_6_0_2, 
-                            lut_rom_addr_6_0_1, lut_rom_addr_6_0_0, 
-                            lut_rom_data_3_0_3, lut_rom_data_3_0_2, 
-                            lut_rom_data_3_0_1, lut_rom_data_3_0_0, vss, vdd)
+extern network na310 (terminal A, B, C, Y, vss, vdd)
+network piece_lut (terminal clk, rst, mask_7_0_7, mask_7_0_6, mask_7_0_5, 
+                            mask_7_0_4, mask_7_0_3, mask_7_0_2, mask_7_0_1, 
+                            mask_7_0_0, mask_select_1_0_1, mask_select_1_0_0, 
+                            x_2_0_2, x_2_0_1, x_2_0_0, y_3_0_3, y_3_0_2, 
+                            y_3_0_1, y_3_0_0, rot_1_0_1, rot_1_0_0, 
+                            piece_type_2_0_2, piece_type_2_0_1, 
+                            piece_type_2_0_0, next_piece, ready, overflow, 
+                            check_start, draw_start, rom_addr_6_0_6, 
+                            rom_addr_6_0_5, rom_addr_6_0_4, rom_addr_6_0_3, 
+                            rom_addr_6_0_2, rom_addr_6_0_1, rom_addr_6_0_0, 
+                            rom_data_3_0_3, rom_data_3_0_2, rom_data_3_0_1, 
+                            rom_data_3_0_0, vss, vdd)
 {
-   net {lut_mask_7_0_7, lut_next_piece};
-   net {lut_mask_7_0_6, lut_mask_6_port};
-   net {lut_mask_7_0_5, lut_mask_5_port};
-   net {lut_mask_7_0_4, lut_mask_4_port};
-   net {lut_mask_7_0_3, lut_mask_3_port};
-   net {lut_mask_7_0_2, lut_mask_2_port};
-   net {lut_mask_7_0_1, lut_mask_1_port};
-   net {lut_mask_7_0_0, lut_mask_0_port};
-   net {lut_rom_addr_6_0_6, lut_piece_type_2_0_2};
-   net {lut_rom_addr_6_0_5, lut_piece_type_2_0_1};
-   net {lut_rom_addr_6_0_4, lut_piece_type_2_0_0};
-   net {lut_rom_addr_6_0_3, lut_rot_1_0_1};
-   net {lut_rom_addr_6_0_2, lut_rot_1_0_0};
-   net {lut_rom_addr_6_0_1, lut_mask_select_1_0_1};
-   net {lut_rom_addr_6_0_0, lut_mask_select_1_0_0};
-   {lut_state_reg_0_inst} dfr11 (lut_next_state_0_port, lut_rst, lut_clk, 
-                                 lut_state_0_port, vss, vdd);
-   {lut_state_reg_1_inst} dfr11 (lut_next_state_1_port, lut_rst, lut_clk, 
-                                 lut_state_1_port, vss, vdd);
-   {lut_state_reg_2_inst} dfr11 (lut_next_state_2_port, lut_rst, lut_clk, 
-                                 lut_state_2_port, vss, vdd);
-   {U40} iv110 (n47, lut_mask_1_port, vss, vdd);
-   {U41} ex210 (n48, n49, n47, vss, vdd);
-   {U42} na210 (lut_rom_data_3_0_0, lut_x_2_0_0, n48, vss, vdd);
-   {U43} na310 (n50, n51, n52, lut_next_state_2_port, vss, vdd);
-   {U44} na210 (lut_state_0_port, n53, n52, vss, vdd);
-   {U45} ex210 (n54, n55, n53, vss, vdd);
-   {U46} na210 (n56, n57, lut_next_state_1_port, vss, vdd);
-   {U47} na210 (lut_state_0_port, n55, n57, vss, vdd);
-   {U48} na210 (n56, n58, lut_next_state_0_port, vss, vdd);
-   {U49} na210 (n59, n60, n58, vss, vdd);
-   {U50} na210 (n50, n61, n59, vss, vdd);
-   {U51} na210 (lut_start, n54, n61, vss, vdd);
-   {U52} na310 (n62, n55, lut_state_2_port, n50, vss, vdd);
-   {U53} na210 (lut_x_2_0_2, n63, n62, vss, vdd);
-   {U54} iv110 (n64, n56, vss, vdd);
-   {U55} na210 (n51, n65, n64, vss, vdd);
-   {U56} na310 (n60, n54, lut_state_1_port, n65, vss, vdd);
-   {U57} na310 (n66, n60, n67, n51, vss, vdd);
-   {U58} no210 (n55, n54, n67, vss, vdd);
-   {U59} iv110 (n68, lut_mask_6_port, vss, vdd);
-   {U60} ex210 (lut_y_3_0_3, n69, n68, vss, vdd);
-   {U61} na210 (lut_y_3_0_2, n70, n69, vss, vdd);
-   {U62} ex210 (n70, lut_y_3_0_2, lut_mask_5_port, vss, vdd);
-   {U63} iv110 (n71, lut_mask_4_port, vss, vdd);
-   {U64} ex210 (n72, n73, n71, vss, vdd);
-   {U65} na210 (lut_rom_data_3_0_2, lut_y_3_0_0, n72, vss, vdd);
-   {U66} ex210 (lut_y_3_0_0, lut_rom_data_3_0_2, lut_mask_3_port, vss, vdd);
-   {U67} ex210 (n63, lut_x_2_0_2, lut_mask_2_port, vss, vdd);
-   {U68} ex210 (lut_x_2_0_0, lut_rom_data_3_0_0, lut_mask_0_port, vss, vdd);
-   {U69} no310 (n74, n54, n75, lut_error_side, vss, vdd);
-   {U70} iv110 (lut_x_2_0_2, n75, vss, vdd);
-   {U71} na310 (n60, n55, n63, n74, vss, vdd);
-   {U72} na210 (n76, n77, n63, vss, vdd);
-   {U73} na310 (lut_rom_data_3_0_0, n49, lut_x_2_0_0, n77, vss, vdd);
-   {U74} ex210 (lut_rom_data_3_0_1, lut_x_2_0_1, n49, vss, vdd);
-   {U75} na210 (lut_rom_data_3_0_1, lut_x_2_0_1, n76, vss, vdd);
-   {U76} no310 (n78, n54, n66, lut_error_bot, vss, vdd);
-   {U77} na310 (lut_y_3_0_2, n70, lut_y_3_0_3, n66, vss, vdd);
-   {U78} na210 (n79, n80, n70, vss, vdd);
-   {U79} na310 (lut_rom_data_3_0_2, n73, lut_y_3_0_0, n80, vss, vdd);
-   {U80} ex210 (lut_rom_data_3_0_3, lut_y_3_0_1, n73, vss, vdd);
-   {U81} na210 (lut_rom_data_3_0_3, lut_y_3_0_1, n79, vss, vdd);
-   {U82} na210 (lut_state_1_port, n60, n78, vss, vdd);
-   {U83} no310 (n60, n55, n54, lut_ready, vss, vdd);
-   {U84} iv110 (lut_state_2_port, n54, vss, vdd);
-   {U85} iv110 (lut_state_1_port, n55, vss, vdd);
-   {U86} iv110 (lut_state_0_port, n60, vss, vdd);
+   net {mask_7_0_7, next_piece};
+   net {mask_7_0_6, mask_6_port};
+   net {mask_7_0_5, mask_5_port};
+   net {mask_7_0_4, mask_4_port};
+   net {mask_7_0_3, mask_3_port};
+   net {mask_7_0_2, mask_2_port};
+   net {mask_7_0_1, mask_1_port};
+   net {mask_7_0_0, mask_0_port};
+   net {ready, N42};
+   net {overflow, overflow_port};
+   net {rom_addr_6_0_6, piece_type_2_0_2};
+   net {rom_addr_6_0_5, piece_type_2_0_1};
+   net {rom_addr_6_0_4, piece_type_2_0_0};
+   net {rom_addr_6_0_3, rot_1_0_1};
+   net {rom_addr_6_0_2, rot_1_0_0};
+   net {rom_addr_6_0_1, mask_select_1_0_1};
+   net {rom_addr_6_0_0, mask_select_1_0_0};
+   {state_reg_0_inst} dfr11 (next_state_0_port, rst, clk, state_0_port, vss, 
+                             vdd);
+   {state_reg_1_inst} dfr11 (next_state_1_port, rst, clk, state_1_port, vss, 
+                             vdd);
+   {state_reg_2_inst} dfr11 (next_state_2_port, rst, clk, state_2_port, vss, 
+                             vdd);
+   {overflow1_reg} dfr11 (next_overflow, rst, clk, overflow_port, vss, vdd);
+   {U48} na210 (n54, n55, next_state_2_port, vss, vdd);
+   {U49} mu111 (state_1_port, n56, n57, n54, vss, vdd);
+   {U50} na210 (state_1_port, state_0_port, n56, vss, vdd);
+   {U51} na310 (n58, n59, n60, next_state_1_port, vss, vdd);
+   {U52} ex210 (n61, state_1_port, n60, vss, vdd);
+   {U53} iv110 (n62, n59, vss, vdd);
+   {U54} na310 (n63, n64, x_2_0_2, n58, vss, vdd);
+   {U55} na210 (n55, n65, next_state_0_port, vss, vdd);
+   {U56} na210 (state_1_port, n61, n65, vss, vdd);
+   {U57} iv110 (state_0_port, n61, vss, vdd);
+   {U58} no210 (n62, n63, n55, vss, vdd);
+   {U59} no210 (n66, n67, n62, vss, vdd);
+   {U60} no210 (draw_start, check_start, n66, vss, vdd);
+   {U61} na210 (n68, n69, next_overflow, vss, vdd);
+   {U62} na210 (n70, n63, n69, vss, vdd);
+   {U63} no210 (n57, state_0_port, n63, vss, vdd);
+   {U64} iv110 (state_2_port, n57, vss, vdd);
+   {U65} iv110 (n71, n70, vss, vdd);
+   {U66} mu111 (n72, n73, state_1_port, n71, vss, vdd);
+   {U67} na310 (y_3_0_3, n74, y_3_0_2, n73, vss, vdd);
+   {U68} na210 (n64, x_2_0_2, n72, vss, vdd);
+   {U69} na210 (overflow_port, N42, n68, vss, vdd);
+   {U70} ex210 (n75, n76, mask_6_port, vss, vdd);
+   {U71} na210 (y_3_0_2, n74, n76, vss, vdd);
+   {U72} iv110 (y_3_0_3, n75, vss, vdd);
+   {U73} ex210 (n74, y_3_0_2, mask_5_port, vss, vdd);
+   {U74} na210 (n77, n78, n74, vss, vdd);
+   {U75} na310 (rom_data_3_0_2, n79, y_3_0_0, n78, vss, vdd);
+   {U76} na210 (rom_data_3_0_3, y_3_0_1, n77, vss, vdd);
+   {U77} ex210 (n80, n79, mask_4_port, vss, vdd);
+   {U78} ex210 (rom_data_3_0_3, y_3_0_1, n79, vss, vdd);
+   {U79} iv110 (n81, n80, vss, vdd);
+   {U80} na210 (rom_data_3_0_2, y_3_0_0, n81, vss, vdd);
+   {U81} ex210 (y_3_0_0, rom_data_3_0_2, mask_3_port, vss, vdd);
+   {U82} ex210 (n64, x_2_0_2, mask_2_port, vss, vdd);
+   {U83} na210 (n82, n83, n64, vss, vdd);
+   {U84} na310 (rom_data_3_0_0, n84, x_2_0_0, n83, vss, vdd);
+   {U85} na210 (rom_data_3_0_1, x_2_0_1, n82, vss, vdd);
+   {U86} ex210 (n85, n84, mask_1_port, vss, vdd);
+   {U87} ex210 (rom_data_3_0_1, x_2_0_1, n84, vss, vdd);
+   {U88} iv110 (n86, n85, vss, vdd);
+   {U89} na210 (rom_data_3_0_0, x_2_0_0, n86, vss, vdd);
+   {U90} ex210 (x_2_0_0, rom_data_3_0_0, mask_0_port, vss, vdd);
+   {U91} iv110 (n67, N42, vss, vdd);
+   {U92} na310 (state_0_port, state_2_port, state_1_port, n67, vss, vdd);
 }
 
 
--- LUT/VHDL/piece_lut_SYNTH.vhd
+++ LUT/VHDL/piece_lut_SYNTH.vhd
@@ -6,24 +6,24 @@ use CellsLib.CellsLib_DECL_PACK.all;
 
 architecture synthesised of piece_lut is
 
-   component iv110
-      port( A : in std_logic;  Y : out std_logic);
-   end component;
-   
-   component no310
+   component na310
       port( A, B, C : in std_logic;  Y : out std_logic);
    end component;
    
-   component na210
-      port( A, B : in std_logic;  Y : out std_logic);
+   component iv110
+      port( A : in std_logic;  Y : out std_logic);
    end component;
    
    component ex210
       port( A, B : in std_logic;  Y : out std_logic);
    end component;
    
-   component na310
-      port( A, B, C : in std_logic;  Y : out std_logic);
+   component na210
+      port( A, B : in std_logic;  Y : out std_logic);
+   end component;
+   
+   component mu111
+      port( A, B, S : in std_logic;  Y : out std_logic);
    end component;
    
    component no210
@@ -34,79 +34,75 @@ architecture synthesised of piece_lut is
       port( D, R, CK : in std_logic;  Q : out std_logic);
    end component;
    
-   signal lut_mask_6_port, lut_mask_5_port, lut_mask_4_port, lut_mask_3_port, 
-      lut_mask_2_port, lut_mask_0_port, lut_state_2_port, lut_state_1_port, 
-      lut_state_0_port, lut_next_state_2_port, lut_next_state_1_port, 
-      lut_next_state_0_port, n47, n48, n49, n50, n51, n52, n53, n54, n55, n56, 
-      n57, n58, n59, n60, n61, n62, n63, n64, n65, n66, n67, n68, n69, n70, n71
-      , n72, n73, n74, n75, n76, n77, n78, n79, n80, lut_mask_1_port : 
-      std_logic;
+   signal mask_6_port, mask_5_port, mask_4_port, mask_3_port, mask_2_port, 
+      mask_1_port, mask_0_port, overflow_port, state_2_port, state_1_port, 
+      state_0_port, next_state_2_port, next_state_1_port, next_state_0_port, 
+      next_overflow, N42, n54, n55, n56, n57, n58, n59, n60, n61, n62, n63, n64
+      , n65, n66, n67, n68, n69, n70, n71, n72, n73, n74, n75, n76, n77, n78, 
+      n79, n80, n81, n82, n83, n84, n85, n86 : std_logic;
 
 begin
-   lut_mask <= ( lut_next_piece, lut_mask_6_port, lut_mask_5_port, 
-      lut_mask_4_port, lut_mask_3_port, lut_mask_2_port, lut_mask_1_port, 
-      lut_mask_0_port );
-   lut_rom_addr <= ( lut_piece_type(2), lut_piece_type(1), lut_piece_type(0), 
-      lut_rot(1), lut_rot(0), lut_mask_select(1), lut_mask_select(0) );
+   mask <= ( next_piece, mask_6_port, mask_5_port, mask_4_port, mask_3_port, 
+      mask_2_port, mask_1_port, mask_0_port );
+   ready <= N42;
+   overflow <= overflow_port;
+   rom_addr <= ( piece_type(2), piece_type(1), piece_type(0), rot(1), rot(0), 
+      mask_select(1), mask_select(0) );
    
-   lut_state_reg_0_inst : dfr11 port map( D => lut_next_state_0_port, R => 
-                           lut_rst, CK => lut_clk, Q => lut_state_0_port);
-   lut_state_reg_1_inst : dfr11 port map( D => lut_next_state_1_port, R => 
-                           lut_rst, CK => lut_clk, Q => lut_state_1_port);
-   lut_state_reg_2_inst : dfr11 port map( D => lut_next_state_2_port, R => 
-                           lut_rst, CK => lut_clk, Q => lut_state_2_port);
-   U40 : iv110 port map( A => n47, Y => lut_mask_1_port);
-   U41 : ex210 port map( A => n48, B => n49, Y => n47);
-   U42 : na210 port map( A => lut_rom_data(0), B => lut_x(0), Y => n48);
-   U43 : na310 port map( A => n50, B => n51, C => n52, Y => 
-                           lut_next_state_2_port);
-   U44 : na210 port map( A => lut_state_0_port, B => n53, Y => n52);
-   U45 : ex210 port map( A => n54, B => n55, Y => n53);
-   U46 : na210 port map( A => n56, B => n57, Y => lut_next_state_1_port);
-   U47 : na210 port map( A => lut_state_0_port, B => n55, Y => n57);
-   U48 : na210 port map( A => n56, B => n58, Y => lut_next_state_0_port);
-   U49 : na210 port map( A => n59, B => n60, Y => n58);
-   U50 : na210 port map( A => n50, B => n61, Y => n59);
-   U51 : na210 port map( A => lut_start, B => n54, Y => n61);
-   U52 : na310 port map( A => n62, B => n55, C => lut_state_2_port, Y => n50);
-   U53 : na210 port map( A => lut_x(2), B => n63, Y => n62);
-   U54 : iv110 port map( A => n64, Y => n56);
-   U55 : na210 port map( A => n51, B => n65, Y => n64);
-   U56 : na310 port map( A => n60, B => n54, C => lut_state_1_port, Y => n65);
-   U57 : na310 port map( A => n66, B => n60, C => n67, Y => n51);
-   U58 : no210 port map( A => n55, B => n54, Y => n67);
-   U59 : iv110 port map( A => n68, Y => lut_mask_6_port);
-   U60 : ex210 port map( A => lut_y(3), B => n69, Y => n68);
-   U61 : na210 port map( A => lut_y(2), B => n70, Y => n69);
-   U62 : ex210 port map( A => n70, B => lut_y(2), Y => lut_mask_5_port);
-   U63 : iv110 port map( A => n71, Y => lut_mask_4_port);
-   U64 : ex210 port map( A => n72, B => n73, Y => n71);
-   U65 : na210 port map( A => lut_rom_data(2), B => lut_y(0), Y => n72);
-   U66 : ex210 port map( A => lut_y(0), B => lut_rom_data(2), Y => 
-                           lut_mask_3_port);
-   U67 : ex210 port map( A => n63, B => lut_x(2), Y => lut_mask_2_port);
-   U68 : ex210 port map( A => lut_x(0), B => lut_rom_data(0), Y => 
-                           lut_mask_0_port);
-   U69 : no310 port map( A => n74, B => n54, C => n75, Y => lut_error_side);
-   U70 : iv110 port map( A => lut_x(2), Y => n75);
-   U71 : na310 port map( A => n60, B => n55, C => n63, Y => n74);
-   U72 : na210 port map( A => n76, B => n77, Y => n63);
-   U73 : na310 port map( A => lut_rom_data(0), B => n49, C => lut_x(0), Y => 
-                           n77);
-   U74 : ex210 port map( A => lut_rom_data(1), B => lut_x(1), Y => n49);
-   U75 : na210 port map( A => lut_rom_data(1), B => lut_x(1), Y => n76);
-   U76 : no310 port map( A => n78, B => n54, C => n66, Y => lut_error_bot);
-   U77 : na310 port map( A => lut_y(2), B => n70, C => lut_y(3), Y => n66);
-   U78 : na210 port map( A => n79, B => n80, Y => n70);
-   U79 : na310 port map( A => lut_rom_data(2), B => n73, C => lut_y(0), Y => 
-                           n80);
-   U80 : ex210 port map( A => lut_rom_data(3), B => lut_y(1), Y => n73);
-   U81 : na210 port map( A => lut_rom_data(3), B => lut_y(1), Y => n79);
-   U82 : na210 port map( A => lut_state_1_port, B => n60, Y => n78);
-   U83 : no310 port map( A => n60, B => n55, C => n54, Y => lut_ready);
-   U84 : iv110 port map( A => lut_state_2_port, Y => n54);
-   U85 : iv110 port map( A => lut_state_1_port, Y => n55);
-   U86 : iv110 port map( A => lut_state_0_port, Y => n60);
+   state_reg_0_inst : dfr11 port map( D => next_state_0_port, R => rst, CK => 
+                           clk, Q => state_0_port);
+   state_reg_1_inst : dfr11 port map( D => next_state_1_port, R => rst, CK => 
+                           clk, Q => state_1_port);
+   state_reg_2_inst : dfr11 port map( D => next_state_2_port, R => rst, CK => 
+                           clk, Q => state_2_port);
+   overflow1_reg : dfr11 port map( D => next_overflow, R => rst, CK => clk, Q 
+                           => overflow_port);
+   U48 : na210 port map( A => n54, B => n55, Y => next_state_2_port);
+   U49 : mu111 port map( A => state_1_port, B => n56, S => n57, Y => n54);
+   U50 : na210 port map( A => state_1_port, B => state_0_port, Y => n56);
+   U51 : na310 port map( A => n58, B => n59, C => n60, Y => next_state_1_port);
+   U52 : ex210 port map( A => n61, B => state_1_port, Y => n60);
+   U53 : iv110 port map( A => n62, Y => n59);
+   U54 : na310 port map( A => n63, B => n64, C => x(2), Y => n58);
+   U55 : na210 port map( A => n55, B => n65, Y => next_state_0_port);
+   U56 : na210 port map( A => state_1_port, B => n61, Y => n65);
+   U57 : iv110 port map( A => state_0_port, Y => n61);
+   U58 : no210 port map( A => n62, B => n63, Y => n55);
+   U59 : no210 port map( A => n66, B => n67, Y => n62);
+   U60 : no210 port map( A => draw_start, B => check_start, Y => n66);
+   U61 : na210 port map( A => n68, B => n69, Y => next_overflow);
+   U62 : na210 port map( A => n70, B => n63, Y => n69);
+   U63 : no210 port map( A => n57, B => state_0_port, Y => n63);
+   U64 : iv110 port map( A => state_2_port, Y => n57);
+   U65 : iv110 port map( A => n71, Y => n70);
+   U66 : mu111 port map( A => n72, B => n73, S => state_1_port, Y => n71);
+   U67 : na310 port map( A => y(3), B => n74, C => y(2), Y => n73);
+   U68 : na210 port map( A => n64, B => x(2), Y => n72);
+   U69 : na210 port map( A => overflow_port, B => N42, Y => n68);
+   U70 : ex210 port map( A => n75, B => n76, Y => mask_6_port);
+   U71 : na210 port map( A => y(2), B => n74, Y => n76);
+   U72 : iv110 port map( A => y(3), Y => n75);
+   U73 : ex210 port map( A => n74, B => y(2), Y => mask_5_port);
+   U74 : na210 port map( A => n77, B => n78, Y => n74);
+   U75 : na310 port map( A => rom_data(2), B => n79, C => y(0), Y => n78);
+   U76 : na210 port map( A => rom_data(3), B => y(1), Y => n77);
+   U77 : ex210 port map( A => n80, B => n79, Y => mask_4_port);
+   U78 : ex210 port map( A => rom_data(3), B => y(1), Y => n79);
+   U79 : iv110 port map( A => n81, Y => n80);
+   U80 : na210 port map( A => rom_data(2), B => y(0), Y => n81);
+   U81 : ex210 port map( A => y(0), B => rom_data(2), Y => mask_3_port);
+   U82 : ex210 port map( A => n64, B => x(2), Y => mask_2_port);
+   U83 : na210 port map( A => n82, B => n83, Y => n64);
+   U84 : na310 port map( A => rom_data(0), B => n84, C => x(0), Y => n83);
+   U85 : na210 port map( A => rom_data(1), B => x(1), Y => n82);
+   U86 : ex210 port map( A => n85, B => n84, Y => mask_1_port);
+   U87 : ex210 port map( A => rom_data(1), B => x(1), Y => n84);
+   U88 : iv110 port map( A => n86, Y => n85);
+   U89 : na210 port map( A => rom_data(0), B => x(0), Y => n86);
+   U90 : ex210 port map( A => x(0), B => rom_data(0), Y => mask_0_port);
+   U91 : iv110 port map( A => n67, Y => N42);
+   U92 : na310 port map( A => state_0_port, B => state_2_port, C => 
+                           state_1_port, Y => n67);
 
 end synthesised;
 
