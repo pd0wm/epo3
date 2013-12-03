2ac54ee5f6fe59b7e2b0bbae8ab9155b47eabe49
santorwarmerdam <santorwarmerdam@hotmail.com>
2013-12-03 19:59:52 +0100

    Name changes to de_piece and npg, de_piece changed and go with the flow added
    Didn't actually change anything other than de_piece and npg, still wants
    to commit, why?

--- next_piece_generator/mux2_1_entity.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-
-Entity npg_mux2_1 is
-        port(
-          out1 : out std_logic;
-          sel, in1, in2 : in std_logic
-        );
-      end entity npg_mux2_1;
