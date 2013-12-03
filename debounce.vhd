42efefe548a4c030c2122ce65d3bf209d417a2b6
Willem Melching <willem.melching@gmail.com>
2013-12-03 10:41:10 +0100

    Updated schematic

--- debounce.vhd
+++ debounce.vhd
@@ -1,15 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity debounce is
-	port(
-		clk         : in  std_logic;
-		rst         : in  std_logic;
+    port(
+        clk         : in  std_logic;
+        rst         : in  std_logic;
 
-		inputs      : in  std_logic_vector(7 downto 0);
-		output      : out std_logic_vector(7 downto 0);
+        vga_clock : in std_logic;
 
-		random_seed : out std_logic
-	);
+        inputs      : in  std_logic_vector(7 downto 0);
+        output      : out std_logic_vector(7 downto 0);
+
+        random_seed : out std_logic
+    );
 end debounce;
 
 
