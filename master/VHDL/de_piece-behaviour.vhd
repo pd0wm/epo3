189e5b0362e73f6cb551eb2c330a6a9b5fc20471
Willem Melching <willem.melching@gmail.com>
2013-12-04 00:56:34 +0100

    redesigned depiece

--- master/VHDL/de_piece-behaviour.vhd
+++ master/VHDL/de_piece-behaviour.vhd
@@ -4,19 +4,41 @@ use ieee.numeric_std.all;
 
 architecture behaviour of de_piece is
 	type sending_state is (waiting, writing, done, lut_lookup);
+
 	signal state, new_state                 : sending_state;
-	signal current_block, new_current_block : unsigned(1 downto 0);
+	signal cur_block, new_block             : unsigned(1 downto 0);
+	signal cur_ready, new_ready             : std_logic;
+	signal cur_addr, new_addr               : std_logic_vector(7 downto 0);
+	signal cur_mask_select, new_mask_select : std_logic_vector(2 downto 0);
+	signal cur_lut_start, new_lut_start     : std_logic;
+	signal cur_data, new_data               : std_logic;
+	signal cur_write, new_write             : std_logic;
+
 begin
 	lbl1 : process(clk)
 	begin
 		if (rising_edge(clk)) then
 			if rst = '1' then
-				state         <= waiting;
-				current_block <= "00";
+				state     <= waiting;
+				cur_block <= "00";
 			else
-				state         <= new_state;
-				current_block <= new_current_block;
+				state           <= new_state;
+				cur_block       <= new_block;
+				cur_ready       <= new_ready;
+				cur_addr        <= new_addr;
+				cur_mask_select <= new_mask_select;
+				cur_lut_start   <= new_lut_start;
+				cur_data        <= new_data;
+				cur_write       <= new_write;
 			end if;
+
+			ready       <= cur_ready;
+			mask_select <= cur_mask_select;
+			lut_start   <= cur_lut_start;
+			write       <= cur_write;
+			addr        <= cur_addr;
+			data        <= cur_data;
+
 		end if;
 	end process;
 
@@ -24,63 +46,70 @@ begin
 	begin
 		case state is
 			when waiting =>
-				write             <= 'Z';
-				ready             <= '0';
-				addr              <= "ZZZZZZZZ";
-				mask_select       <= "ZZ";
-				lut_start         <= '0';
-				new_current_block <= "00";
-				data              <= 'Z';
+				new_write       <= 'Z';
+				new_ready       <= '0';
+				new_addr        <= "ZZZZZZZZ";
+				new_mask_select <= "ZZ";
+				new_lut_start   <= '0';
+				new_block       <= "00";
+				new_data        <= 'Z';
+
 				if (start = '1') then
 					new_state <= lut_lookup;
 				else
 					new_state <= waiting;
 				end if;
 
-			when writing =>
-				write             <= '1';
-				ready             <= '0';
-				addr              <= mask;
-				data              <= draw_erase;
-				new_current_block <= current_block + 1;
-				lut_start         <= '0';
-				if (current_block = 3) then
-					new_state <= done;
-				else
-					new_state <= lut_lookup;
-				end if;
-				mask_select <= "ZZ";
-
 			when lut_lookup =>
-				write             <= '0';
-				ready             <= '0';
-				addr              <= "ZZZZZZZZ";
-				data              <= 'Z';
-				lut_start         <= '1';
-				mask_select       <= std_logic_vector(current_block);
-				new_current_block <= current_block;
+				new_write       <= '0';
+				new_lut_start   <= '1';
+				new_mask_select <= std_logic_vector(cur_block);
+
+				new_ready <= cur_ready;
+				new_addr  <= cur_addr;
+				new_data  <= cur_data;
+				new_block <= cur_block;
+
 				if (lut_ready = '1') then
 					new_state <= writing;
 				else
 					new_state <= lut_lookup;
 				end if;
 
+			when writing =>
+				new_write       <= '1';
+				new_ready       <= cur_ready;
+				new_addr        <= mask;
+				new_data        <= draw_erase;
+				new_block       <= cur_block + 1;
+				new_lut_start   <= '0';
+				new_mask_select <= "ZZ";
+
+				if (cur_block = 3) then
+					new_state <= done;
+				else
+					new_state <= lut_lookup;
+				end if;
+
 			when done =>
-				write             <= 'Z';
-				ready             <= '1';
-				lut_start         <= '0';
-				mask_select       <= "ZZ";
-				new_current_block <= "00";
-				addr              <= "ZZZZZZZZ";
-				data              <= 'Z';
 				if (start = '1') then
 					new_state <= done;
 				else
 					new_state <= waiting;
 				end if;
+
+				new_write       <= 'Z';
+				new_ready       <= '1';
+				new_lut_start   <= '0';
+				new_mask_select <= "ZZ";
+				new_block       <= "00";
+				new_addr        <= "ZZZZZZZZ";
+				new_data        <= 'Z';
+
 		end case;
 	end process;
 end behaviour;
+
 
 
 
