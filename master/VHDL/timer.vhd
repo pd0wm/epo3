2e6072e19fa87b5d51c830e8bd36e7f2bd948daa
Willem Melching <willem.melching@gmail.com>
2013-12-05 20:24:52 +0100

    timer redesign

--- master/VHDL/timer.vhd
+++ master/VHDL/timer.vhd
@@ -3,13 +3,16 @@ use IEEE.std_logic_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 
 entity timer is
-   port(timer_clk      :in    std_logic;
-	timer_vga_clk  :in		std_logic;
-        timer_rst      :in    std_logic;
-	timer_ctr_rst : in std_logic;
-	timer_time	 :in 	std_logic_vector(7 downto 0);
-	timer_start	 :in 	std_logic;
-        timer_done		 :out   std_logic);
+	port(
+		clk     : in  std_logic;
+		vga_clk : in  std_logic;
+
+		rst     : in  std_logic;
+		ctr_rst : in  std_logic;
+
+		time    : in  std_logic_vector(7 downto 0);
+		start   : in  std_logic;
+		ready   : out std_logic);
 end timer;
 
 
