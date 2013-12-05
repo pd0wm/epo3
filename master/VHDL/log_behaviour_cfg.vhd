ef560f224c1af0b9bf5aaec661071c63aa3bc09a
Willem Melching <willem.melching@gmail.com>
2013-12-05 22:51:49 +0100

    Added score to gwtf project

---
+++ master/VHDL/log_behaviour_cfg.vhd
@@ -0,0 +1,6 @@
+configuration log_behaviour_cfg of log is
+   for behaviour
+   end for;
+end log_behaviour_cfg;
+
+
---
+++ master/VHDL/score_tb_behaviour_cfg.vhd
@@ -0,0 +1,8 @@
+configuration score_tb_behaviour_cfg of score_tb is
+   for behaviour
+      for all: score use configuration work.score_behaviour_cfg;
+      end for;
+   end for;
+end score_tb_behaviour_cfg;
+
+
