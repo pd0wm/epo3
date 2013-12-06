0168ff1f6ed372a42dea4488abfed4a4aebc24c6
Willem Melching <willem.melching@gmail.com>
2013-12-06 12:19:49 +0100

    added move right

--- master/VHDL/controller-controller_arch.vhd
+++ master/VHDL/controller-controller_arch.vhd
@@ -5,7 +5,7 @@ use work.vga_params.all;
 
 architecture controller_arch of controller is
 	type state_type
-	is (reset, init, rotate_cw_1, rotate_ccw_1, move_left_1, move_left_2, move_left_3,move_left_4,move_left_5,move_left_6, move_left_7,move_left_8,move_left_9,move_left_10,move_left_11,move_right_1, first_draw_1, first_draw_2, first_draw_3, first_draw_4, drop_timer_reset, gen_piece_1, gen_piece_2, collision_1, collision_3, collision_4, collision_5, draw, kernel_panic, lock_overflow, reset_timers_a_1, reset_timers_a_2, clear_shift_1, clear_shift_2, space_1, space_2, space_3, space_4, space_5, space_6, put_back_1, put_back_3, put_back_4, move_down_1, move_down_3, move_down_4, reset_timers_b_1, reset_timers_b_2, drop_overflow, key, lock_timer_start, game_over);
+	is (reset, init, rotate_cw_1, rotate_ccw_1, move_left_1, move_left_2, move_left_3, move_left_4, move_left_5, move_left_6, move_left_7, move_left_8, move_left_9, move_left_10, move_left_11, move_right_1, move_right_2,move_right_3,move_right_4,first_draw_1, first_draw_2, first_draw_3, first_draw_4, drop_timer_reset, gen_piece_1, gen_piece_2, collision_1, collision_3, collision_4, collision_5, draw, kernel_panic, lock_overflow, reset_timers_a_1, reset_timers_a_2, clear_shift_1, clear_shift_2, space_1, space_2, space_3, space_4, space_5, space_6, put_back_1, put_back_3, put_back_4, move_down_1, move_down_3, move_down_4, reset_timers_b_1, reset_timers_b_2, drop_overflow, key, lock_timer_start, game_over);
 	signal cur_state, next_state : state_type;
 
 	signal cur_piece, new_cur_piece     : std_logic_vector(2 downto 0);
@@ -50,7 +50,7 @@ architecture controller_arch of controller is
 	signal cur_timer_2_time      : std_logic_vector(7 downto 0);
 	signal cur_timer_2_start     : std_logic;
 	signal cur_timer_2_reset     : std_logic;
-	signal inv_inputs : std_logic_vector(7 downto 0);
+	signal inv_inputs            : std_logic_vector(7 downto 0);
 
 begin
 	process(clk, rst)
@@ -88,7 +88,7 @@ begin
 				cur_timer_2_time      <= new_timer_2_time;
 				cur_timer_2_start     <= new_timer_2_start;
 				cur_timer_2_reset     <= new_timer_2_reset;
-				inv_inputs <= not inputs;
+				inv_inputs            <= not inputs;
 			end if;
 		end if;
 
@@ -426,57 +426,59 @@ begin
 
 				next_state <= draw;
 
-			when rotate_cw_1     =>
+			when rotate_cw_1 =>
 				next_state <= rotate_ccw_1;
-				
+
 			when rotate_ccw_1 =>
 				next_state <= move_left_1;
-				
+
 			when move_left_1 =>
-			
 				if (inv_inputs(0) = '1') then
 					next_state <= move_left_2;
 				else
 					next_state <= move_right_1;
 				end if;
-				
+
 			when move_left_2 =>
 				-- first erase current piece
-				
-				new_draw_erase_draw <= '0'; -- erase
+
+				new_draw_erase_draw  <= '0'; -- erase
 				new_draw_erase_start <= '1'; --start
-				
-				
+
+
 				next_state <= move_left_3;
-				
+
 			when move_left_3 =>
 				-- wait for erasse ready 
 				if (draw_erase_ready = '1') then
 					next_state <= move_left_4;
 				else
-					next_state  <= move_left_3;
-				end if;	
-				
-			when move_left_4 => 
+					next_state <= move_left_3;
+				end if;
+
+			when move_left_4 =>
 				-- calculate new position				
 				new_draw_erase_start <= '0';
 				new_cur_x_new        <= std_logic_vector(unsigned(new_cur_x) - 1);
-				
+				new_cur_y_new        <= cur_y;
+				new_cur_rot_new      <= cur_rot;
+
 				next_state <= move_left_5;
-			when move_left_5 => 
+			
+			when move_left_5 =>
 				-- create mask for new position
-				
+
 				-- Generate mask for cur_piece
-				new_lut_rot        <= new_cur_rot;
+				new_lut_rot        <= new_cur_rot_new;
 				new_lut_x          <= new_cur_x_new;
-				new_lut_y          <= new_cur_y;
+				new_lut_y          <= new_cur_y_new;
 				new_lut_piece_type <= new_cur_piece;
-				
+
 				new_check_start <= '1';
-				
+
 				next_state <= move_left_6;
-				
-			when move_left_6 => 
+
+			when move_left_6 =>
 				-- Wait for check
 				if (check_ready = '1') then
 					next_state <= move_left_7;
@@ -484,53 +486,83 @@ begin
 					next_state <= move_left_6;
 				end if;
 
-			when move_left_7 => 
+			when move_left_7 =>
 				new_check_start <= '0';
 				if (check_empty = '1') then
 					next_state <= move_left_8;
 				else
 					next_state <= move_left_9;
 				end if;
-			
-			when move_left_8 => 
+
+			when move_left_8 =>
 				-- Save new coordinates
-				new_cur_x <= new_cur_x_new;
-				
+				new_cur_x   <= new_cur_x_new;
+				new_cur_y   <= new_cur_y_new;
+				new_cur_rot <= new_cur_rot_new;
+
 				next_state <= move_left_9;
-				
-			when move_left_9 => 
+
+			when move_left_9 =>
 				-- Generate mask for cur_piece
 				new_lut_rot        <= new_cur_rot;
 				new_lut_x          <= new_cur_x;
 				new_lut_y          <= new_cur_y;
 				new_lut_piece_type <= new_cur_piece;
-				
-				new_draw_erase_draw <= '1'; --draw
+
+				new_draw_erase_draw  <= '1'; --draw
 				new_draw_erase_start <= '1';
-				
+
 				next_state <= move_left_10;
-				
-			when move_left_10 => 
+
+			when move_left_10 =>
 				if (draw_erase_ready = '1') then
 					next_state <= move_left_11;
 				else
 					next_state <= move_left_10;
 				end if;
-				
-			when move_left_11 => 
+
+			when move_left_11 =>
 				new_draw_erase_start <= '0';
-				
+
 				if (inv_inputs = "00000000") then
 					next_state <= draw;
 				else
-					next_state <= move_left_11;	
+					next_state <= move_left_11;
 				end if;
-				
 
-				
-			when move_right_1 => 
-				next_state <= draw;
-				
+			when move_right_1 =>
+				if (inv_inputs(1) = '1') then
+					next_state <= move_left_2;
+				else
+					next_state <= draw;
+				end if;
+
+			when move_right_2 =>
+				-- first erase current piece
+
+				new_draw_erase_draw  <= '0'; -- erase
+				new_draw_erase_start <= '1'; --start
+
+
+				next_state <= move_right_3;
+
+			when move_right_3 =>
+				-- wait for erasse ready 
+				if (draw_erase_ready = '1') then
+					next_state <= move_right_4;
+				else
+					next_state <= move_right_3;
+				end if;
+
+			when move_right_4 =>
+				-- calculate new position				
+				new_draw_erase_start <= '0';
+				new_cur_x_new        <= std_logic_vector(unsigned(new_cur_x) + 1);
+				new_cur_y_new        <= cur_y;
+				new_cur_rot_new      <= cur_rot;
+
+				next_state <= move_left_5;
+
 			when kernel_panic =>
 				-- Kill it!
 				next_state <= game_over;
