4710765450aa5830fc3fd42d635845a57f7c60bc
Willem Melching <willem.melching@gmail.com>
2013-12-11 23:10:38 +0100

    controller redesign with new calculator

--- master/VHDL/controller_calc.vhd
+++ master/VHDL/controller_calc.vhd
@@ -2,9 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity controller_calc is
 	port(
-		clk               : in  std_logic;
-		rst               : in  std_logic
+		old_x              : in  std_logic_vector(2 downto 0);
+		old_y              : in  std_logic_vector(3 downto 0);
+		old_rot            : in  std_logic_vector(1 downto 0);
 
+		new_x              : out std_logic_vector(2 downto 0);
+		new_y              : out std_logic_vector(3 downto 0);
+		new_rot            : out std_logic_vector(1 downto 0);
+
+		add_sub, rot, x, y : in  std_logic
 	);
 end controller_calc;
 
