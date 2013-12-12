7ed74e21443dffa5baa4cd891f55672bf7407950
Willem Melching <willem.melching@gmail.com>
2013-12-12 14:30:10 +0100

    fixed lut

--- master/VHDL/adder_y.vhd
+++ master/VHDL/adder_y.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.ALL;
 
 entity adder_y is
    port(y  :in    std_logic_vector(3 downto 0);
-        rom_data:in    std_logic_vector(1 downto 0);
+        rom_data:in    std_logic_vector(3 downto 0);
 	next_piece: in std_logic;
         y_out :out   std_logic_vector(4 downto 0));
 end adder_y;
