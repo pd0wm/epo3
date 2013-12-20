485f1418b4cbc5f45f4bdb31ad931621aa5e5594
Wessel <wessel_almere@hotmail.com>
2013-12-20 12:04:58 +0100

    new sim

--- master/VHDL/timer_tb_timer_tb_behav_cfg_extr.vhd
+++
@@ -1,8 +0,0 @@
-configuration timer_tb_timer_tb_behav_cfg_extr of timer_tb is
-   for timer_tb_behav
-      for all: timer use configuration work.timer_extracted_cfg;
-      end for;
-   end for;
-end timer_tb_timer_tb_behav_cfg_extr;
-
-
